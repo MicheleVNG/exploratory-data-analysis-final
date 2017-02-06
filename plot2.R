bb26fc758a7d9275bb29f4758e7e1d5cc72dde88
Michele Vangelista <m.vangelista@gmail.com>
2017-02-06 21:18:56 +0100

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,28 @@
+# Read the data
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+df <- merge(NEI, SCC, by = "SCC")
+
+# Plot 2
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510")
+# from 1999 to 2008? Use the base plotting system to make a plot answering this question.
+
+library(dplyr)
+
+graphData <- NEI[NEI$fips == "24510", c("Emissions", "year")]
+graphData <- graphData %>% 
+	group_by(year) %>% 
+	summarize(sum(Emissions)) %>% 
+	rename(totalEmissions = `sum(Emissions)`)
+
+png(filename = "plot2.png")
+
+with(graphData,
+     plot(year, totalEmissions, type = "b", col = "darkolivegreen", lwd = 2,
+          xaxp = c(1999, 2008, 3), ylim = c(0, max(totalEmissions)),
+          xlab = "Year", ylab = "Total Emissions (tons)",
+          main = "Total PM2.5 emissions\nin Baltimore City, MD")
+     )
+
+
+dev.off()
