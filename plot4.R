dc8b3598cbb2bda69dec427b9c2a9c35b40c89fc
Michele Vangelista <m.vangelista@gmail.com>
2017-02-09 21:44:47 +0100

    Plot 4

---
+++ plot4.R
@@ -0,0 +1,26 @@
+# Read the data
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+
+# Plot 4
+# Across the United States, how have emissions from coal combustion-related
+# sources changed from 1999–2008?
+
+library(dplyr)
+library(ggplot2)
+
+SCC <- SCC[, c("SCC", "Short.Name")]
+graphData <- NEI[, c("SCC", "Emissions", "year")]
+graphData <- merge(graphData, SCC, by = "SCC")
+graphData <- graphData %>% subset(grepl("Comb /", graphData$Short.Name))
+graphData <- graphData %>% subset(grepl("Coal", graphData$Short.Name))
+graphData$Emissions <- graphData$Emissions / 1000
+
+png("plot4.png")
+
+g <- ggplot(data = graphData, mapping = aes(as.factor(year), Emissions))
+g + geom_col() +
+	labs(x = "Year", y = "Total Emissions (thousands of tons)",
+	     title = "Emissions from coal combustion-related\nsources in the United States")
+
+dev.off()
